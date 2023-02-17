a2dad548c899808da9d3cc2f1f2e232e0ff717ed
Md Akib Dargahi <mdakibdargahi1@gmail.com>
2023-02-17 23:16:39 +0530

    initialized firebasse project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBDmeWVKO_pGtmOtVQIFaiHaMaNN7dbPyw',
+  authDomain: 'chat-web-app-30772.firebaseapp.com',
+  projectId: 'chat-web-app-30772',
+  storageBucket: 'chat-web-app-30772.appspot.com',
+  messagingSenderId: '383769098246',
+  appId: '1:383769098246:web:756bd960579947df33ab87',
+};
+
+const app = firebase.initializeApp(config);
