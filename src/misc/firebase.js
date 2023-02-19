0e11ac940d9dcd2afc581399aaf15b8541a0d7f3
Md Akib Dargahi <mdakibdargahi1@gmail.com>
2023-02-19 16:46:27 +0530

    added sign in page  and signed in with facebook/google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,8 +1,12 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyBDmeWVKO_pGtmOtVQIFaiHaMaNN7dbPyw',
   authDomain: 'chat-web-app-30772.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-30772-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'chat-web-app-30772',
   storageBucket: 'chat-web-app-30772.appspot.com',
   messagingSenderId: '383769098246',
@@ -10,3 +14,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
