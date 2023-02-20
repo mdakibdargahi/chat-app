58bf6f21fe24aaf3328832a8750da6df75b6f870
Md Akib Dargahi <mdakibdargahi1@gmail.com>
2023-02-20 19:59:40 +0530

    started building sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DashBoardToggle from './dashboard/DashBoardToggle';
+
+const Sidebar = () => {
+  return (
+    <div className="h-100 pt-2">
+      <div>
+        <DashBoardToggle />
+      </div>
+      Bottom
+    </div>
+  );
+};
+
+export default Sidebar;
